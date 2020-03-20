49448466c05d2b381062c7b00cdc372bff9071d7
Dimlt <59270387+Dimlt@users.noreply.github.com>
2020-03-21 01:13:26 +0800

    APlayer插件的引入

---
+++ js/aplayer.js
@@ -0,0 +1,26 @@
+jQuery(function($){
+	const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+	fixed: true,
+	listFolded: true,
+	listMaxHeight: 90,
+	lrcType: 3,
+    audio: [
+        {
+            name: 'name1',
+            artist: 'artist1',
+            url: 'AKEN - MAMAMOO-WIND FLOWER（AKEN Remix）.mp3',
+            cover: 'cover1.jpg',
+            //lrc: 'lrc1.lrc',
+        },
+        {
+            name: 'name2',
+            artist: 'artist2',
+            url: 'url2.mp3',
+            cover: 'cover2.jpg',
+            lrc: 'lrc2.lrc',
+        }
+    ]
+});
+	
+});
