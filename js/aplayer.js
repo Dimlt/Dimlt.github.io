ebb8aabb73a0765665594a096700e53a004bfb9f
Dimlt <59270387+Dimlt@users.noreply.github.com>
2020-03-21 01:22:12 +0800

    Add files via upload

--- js/aplayer.js
+++ js/aplayer.js
@@ -11,7 +11,7 @@ jQuery(function($){
             artist: 'artist1',
             url: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/AKEN - MAMAMOO-WIND FLOWER（AKEN Remix）.mp3',
             cover: 'cover1.jpg',
-            //lrc: 'lrc1.lrc',
+            lrc: 'lrc1.lrc',
         },
         {
             name: 'name2',
