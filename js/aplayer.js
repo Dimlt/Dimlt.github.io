d5d4f9fc229eeeb43896827bac58dcfc9bc3c3f7
Dimlt <59270387+Dimlt@users.noreply.github.com>
2020-03-21 02:47:55 +0800

    Add files via upload

--- js/aplayer.js
+++ js/aplayer.js
@@ -7,11 +7,18 @@ jQuery(function($){
 	lrcType: 3,
     audio: [
         {
-            name: 'Wind flower',
-            artist: 'Mamamoo',
+            name: 'Once upon a Time',
+            artist: 'Max Oazo _ Moonessa',
             url: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/Max Oazo _ Moonessa - Once upon a Time.mp3',
             cover: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/Mamamoo-Wind flower.jpg',
             lrc: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/Max Oazo _ Moonessa - Once upon a Time.lrc',
+        },
+		{
+            name: 'Wind flower',
+            artist: 'MAMAMOO',
+            url: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/MAMAMOO-WIND FLOWER（AKEN Remix）-AKEN.mp3',
+            cover: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/Mamamoo-Wind flower.jpg',
+            lrc: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/MAMAMOO-WIND FLOWER（AKEN Remix）-AKEN.lrc',
         }
     ]
 });
