2287074945db0512cc20b24f96677a48c1628da6
Dimlt <59270387+Dimlt@users.noreply.github.com>
2020-03-21 03:01:19 +0800

    Add files via upload

--- js/aplayer.js
+++ js/aplayer.js
@@ -6,19 +6,19 @@ jQuery(function($){
 	listMaxHeight: 90,
 	lrcType: 3,
     audio: [
-        {
-            name: 'Once upon a Time',
-            artist: 'Max Oazo _ Moonessa',
-            url: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/Max Oazo _ Moonessa - Once upon a Time.mp3',
-            cover: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/Mamamoo-Wind flower.jpg',
-            lrc: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/Once Upon A Time-Max Oazo,Moonessa.lrc',
-        },
 		{
             name: 'Wind flower',
             artist: 'MAMAMOO',
             url: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/MAMAMOO-WIND FLOWER（AKEN Remix）-AKEN.mp3',
             cover: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/Mamamoo-Wind flower.jpg',
             lrc: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/MAMAMOO-WIND FLOWER（AKEN Remix）-AKEN.lrc',
+        },
+		{
+            name: 'Once upon a Time',
+            artist: 'Max Oazo _ Moonessa',
+            url: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/Max Oazo _ Moonessa - Once upon a Time.mp3',
+            cover: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/Mamamoo-Wind flower.jpg',
+            lrc: 'https://cdn.jsdelivr.net/gh/dimlt/dimlt.github.io/musics/Once Upon A Time-Max Oazo,Moonessa.lrc',
         }
     ]
 });
