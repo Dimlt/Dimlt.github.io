e4226be785f993af1a47c37242a59a248e353492
Dimlt <59270387+Dimlt@users.noreply.github.com>
2020-03-21 14:21:49 +0800

    Add files via upload

---
+++ musics/js/others.js
@@ -0,0 +1,45 @@
+// 存储最基础的导航栏class属性值
+var navbar_color_base;
+
+jQuery(function($){
+	
+	// 下拉列表鼠标移入时自动下拉
+	$('li.dropdown').mouseover(function(){$(this).addClass('open');}).mouseout(function(){$(this).removeClass('open');});
+
+
+	// 关闭下拉列表的点击功能
+	$(document).off('click.bs.dropdown.data-api');
+
+
+	// 存储最基础的导航栏class值
+	navbar_color_base = $('nav').attr("class");
+	// 设置用户的首次导航栏为黑色
+	if(localStorage.getItem("navbar_inverse") == null){
+		localStorage.setItem("navbar_inverse",1);
+	};
+	// 设置导航栏黑色背景,白色就是默认的颜色,所以不用设置了
+	if(localStorage.getItem("navbar_inverse") == 1){
+		$('nav').attr("class", function(i,origValue){
+			return origValue + " navbar-inverse";
+		});
+	};
+
+});
+
+
+// 点击黑/白按钮切换导航栏颜色,并记录
+function navbar_inverse(){
+	// 获取导航栏的class值,然后增加navbar-inverse
+	$('nav').attr("class", function(i,origValue){
+		if(localStorage.getItem("navbar_inverse") == 0){
+			localStorage.setItem("navbar_inverse",1);
+			return origValue + " navbar-inverse";
+		}else{
+			localStorage.setItem("navbar_inverse",0);
+			return navbar_color_base;
+		}
+	});
+}
+	
+	
+
